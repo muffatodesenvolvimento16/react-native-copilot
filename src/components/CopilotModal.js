35dc5ddd0157ab9e09811d0c45a8d7eb452234fa
muffatodesenvolvimento16 <61416501+muffatodesenvolvimento16@users.noreply.github.com>
2020-02-24 10:12:31 -0300

    Update CopilotModal.js

--- src/components/CopilotModal.js
+++ src/components/CopilotModal.js
@@ -142,7 +142,7 @@ class CopilotModal extends Component<Props, State> {
       arrow.top = tooltip.top - (ARROW_SIZE * 2);
     } else {
       tooltip.bottom = layout.height - (obj.top - MARGIN);
-      arrow.borderTopColor = '#ff;
+      arrow.borderTopColor = this.props.tooltipStyle.backgroundColor;
       arrow.bottom = tooltip.bottom - (ARROW_SIZE * 2);
     }
 
